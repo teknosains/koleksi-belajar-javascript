96a18628b6c08f7bd71853c2f5341398f8f95edf
Codetrash <budykusniadi@gmail.com>
2018-08-16 09:08:19 +0700

    update penjelasan strict mode

--- 04_use_strict.js
+++ 04_use_strict.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 /**
  * Apa sihi itu use strict ? 
@@ -13,4 +13,67 @@
  * pakailah "use strict" 
  * 
  * use strict wajib ditempatkan di baris paling atas sebuah script, gak boleh gak
+ * 
+ * atau bisa juga kita Apply ke sebuah function namun harus di paling atas block lokasinya
  */
+
+ function strictMode() {
+   'use strict';
+
+    //..
+    //..
+ }
+ /**
+  * Masalah yang bisa terjadi, bayangkan kita punya 2 file
+  * 1. library1.js (strict mode)
+  * 2. library2.js (non-strict mode)
+  * 
+  * saat kita mau menggabungkan ke-2 file itu menjadi 1 file misalnya...
+  * maka kita mesti pastikan urutannya bener...yaitu yang strict mode yang paling atas
+  * sebab jika sebaliknya, maka seluruh script nantinya dianggap bukan dalam strict mode
+
+  * oleh karenya direkomendasikan membungkus semua script kita dalam self-invoking 
+  * function seperti ini
+  */
+
+  (function() {
+    'strict mode';
+
+    const PI = 3.14;
+
+    let doThis = function() {
+
+    };
+
+    let doThat = function () {
+
+    };
+
+    // semua kode kita ada ddalam sini
+    // ..
+    // ..
+
+  })();
+
+  // catatan, this context untuk function dalam strict mode, itu by default undefined
+  // dimana dalam normal mode itu refer ke global window object
+  function test() {
+    'use strict';
+    console.log(this); // undefined
+  }
+
+  test();
+
+  // bandingkan dengan ini
+  function test2() {
+    console.log(this) // [object Window]
+  }
+
+  // self-invoking function, this nya merujuk ke [object Window]
+  (function() {
+    'strict mode';
+    console.log(this); // [object Window]
+  })();
+
+  // itulah kenapa kalao kita menulis sebuah script/library dalam strict mode, sebaiknya
+  // di masukan dalam self-invoking function seperti diatas
--- test.js
+++ test.js
@@ -1,13 +1,2 @@
-let url = 'https://xkcd.com/info.0.json';
-function fetchImage(url) {
-  fetch(url, {method: 'get'})
-  .then((result) => {
-      console.log(result);
-  })
-  .catch((err) => {
-    console.log(err.stack);
-  });
-}
 
-fetchImage(url);
-
+console.log(cons)
