8588d08433e1dd77a00f533e5099da44205064e2
Codetrash <budykusniadi@gmail.com>
2018-09-07 15:22:09 +0700

    update mixin

--- 39_mixin.js
+++ 39_mixin.js
@@ -8,6 +8,9 @@
  * 
  * Mixin juga secara singkat adalah: Teknik "memanfaat"-kan method-method di Class lain
  * tanpa harus meng-inherit/extends Class itu
+ * 
+ * Mixin juga berguna untuk membuat dua Class/Object yang berbeda/gk berhubungan bisa 
+ * sharing method yang umum/sama 
  */
 
  // contoh dengan Class ES6. kita mau bikin Library sendiri dengan extens dari library
@@ -66,3 +69,56 @@ Object.assign(Test.prototype, obj);
 console.dir(Test);
 
 // maka lihat sekarang bahwa method a() dan b() sekarang menjadi bagian dari Test.prototype
+
+/**
+ * Mixin untuk Class yang tidak berhubungan
+ * tapi punya 1 method yang umum/sama
+ */
+
+const mixinMakan = {
+  caraMakan: function() {
+    console.log('Pake Mulut');
+  }
+};
+
+function Kucing() {}
+function Burung() {}
+
+// lihat bahwa kita punya 2 Class yang berbeda jenis...tapi keduanya
+// punya satu kesamaan...yaitu makannya pake mulutnya
+
+Object.assign(Kucing.prototype, mixinMakan);
+Object.assign(Burung.prototype, mixinMakan);
+
+/**
+ * Gimana Mixin pada Object literal biasa? simple
+ * Sama saja dengan cara diatas
+ */
+const mixinMakan = {
+  caraMakan: function() {
+    console.log('Pake Mulut');
+  }
+};
+
+let Kucing = {
+  spesies: 'Anggora'
+};
+let Burung = {
+  species: 'Penguin'
+};
+
+
+Object.assign(Kucing, mixinMakan);
+Object.assign(Burung, mixinMakan);
+
+// kalau gk pakai Object.assign bisa? 
+// tentu bisa...kita buat saja factory function sederhana
+function addMixin(obj) {
+  obj.caraMakan = function() {
+    console.log('Pake Mulut');
+  };
+}
+
+addMixin(Kucing);
+
+console.dir(Kucing);
--- test.js
+++ test.js
@@ -1,17 +1,53 @@
-function build(n) {
-  let x = '';
-  for(let i = 1; i <= n; i++) {
-    for (let j = n; j >= 1; j--) {
-      if (j > i) {
-        x +=' ';
-      } else {
-        x += '*';
-      }
-    }
-    x += '\n';
-  }
+let max_line = 32;
+let max_header = 14;
+let max_footer = 6;
+let max_detail = max_line - max_header - max_footer;
 
-  return x;
+let jumlah_row = 7;
+
+
+let fill_detail = [];
+for(let i = 1; i <= jumlah_row; i++) { fill_detail.push('item-' + i); }
+
+function buildHeader() {
+  for(let i = 1; i <= max_header; i++) {
+    console.log('header-' + i);
+  }
 }
 
-console.log(build(4));
+function buildFooter() {
+  for(let i = 1; i <= max_footer; i++) {
+    console.log('footer-' + i);
+  }
+}
+
+// focus disini
+function buildDetail() {
+  let counter = 1;
+  fill_detail.forEach((item, idx) => {
+    if ((counter+max_detail)+1 < max_line) {
+      console.log(counter + '->' + item);
+      counter++;
+    } else {
+      console.log('*************************************');
+      console.log('*************************************');
+      console.log('*************************************');
+      counter = 1;
+    }
+
+  });
+
+  if (fill_detail.length < max_detail) {
+    for (let i = counter; i <= (max_detail); i++) {
+      console.log(i + '***************************************');
+    }
+  } else {
+    for (let i = 1; i <= (max_line - (counter + max_footer)); i++) {
+      console.log(i + '***************************************');
+    }
+  }
+}
+
+buildHeader();
+buildDetail();
+buildFooter();
